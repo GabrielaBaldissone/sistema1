fa042ba555f0f8652ce312bd63f4e170009c9188
Marco Baldissone <marcol2c4@gmail.com>
2022-11-25 12:35:34 -0300

    git ignore

--- db.php
+++ db.php
@@ -1,14 +1,15 @@
 <?php
 
-    function connect(){
-        $host = 'localhost';
-        $dbname = 'datatm_sistema1';
-        $username = 'datatm_sistema1';
-        $password = 'teresita2022';
-        $data = "mysql:host=" . $host . ";dbname=" . $dbname . ";";
-        try {
-            return new PDO($data, $username, $password);
-        } catch (PDOException $e) {
-            return 'Error: ' . $e->getMessage();
-        }
+function connect()
+{
+    $host = 'localhost';
+    $dbname = 'datatm_sistema1';
+    $username = 'root';
+    $password = '';
+    $data = "mysql:host=" . $host . ";dbname=" . $dbname . ";";
+    try {
+        return new PDO($data, $username, $password);
+    } catch (PDOException $e) {
+        return 'Error: ' . $e->getMessage();
     }
+}
