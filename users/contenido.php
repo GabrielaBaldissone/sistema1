29678aa5aa40582ca2c690f229ee89502dcaf7e4
Marco Baldissone <marcol2c4@gmail.com>
2022-12-02 21:10:33 -0300

    developer no edit

--- users/contenido.php
+++ users/contenido.php
@@ -97,14 +97,18 @@ require 'nav.php';
                             <td><?php echo $row['name'] ?></td>
                             <td><?php echo $row['lastname'] ?></td>
                             <td><?php
-                                if ($row['role'] == 1) : echo "Administrador";
+                                if ($row['role'] == 1 && $row['id'] == 10) : echo "Developer";
+                                elseif ($row['role'] == 1 && $row['id'] != 10) : echo "Administrador";
                                 elseif ($row['role'] == 2) : echo "Dirigente";
                                 elseif ($row['role'] == 3) : echo "Invitado";
                                 endif;
                                 ?></td>
                             <td>
                                 <div class="button-group">
-                                    <form style="display:inline-block;" action="read.php" method="post">
+                                    <?php 
+                                    if ($row['id'] != 10){
+                                        ?>
+                                        <form style="display:inline-block;" action="read.php" method="post">
                                         <button type="submit" name="id" value="<?php echo $row['id'] ?>" title="Ver" class="btn btn-info"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye" viewBox="0 0 16 16">
                                                 <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z" />
                                                 <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z" />
@@ -116,9 +120,9 @@ require 'nav.php';
                                                 <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z" />
                                             </svg></button>
                                     </form>
-
-                                <?php
-
+                                        <?php
+                                    }; 
+                                    
                             }
                                 ?>
                                 </div>
